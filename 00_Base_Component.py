6f2ba8659867b543de8a177f39ea8723e7c28345
xoNacho <hales.nikau@awatapu.school.nz>
2021-08-05 13:14:25 +1200

    commit changes

--- 00_Base_Component.py
+++ 00_Base_Component.py
@@ -30,8 +30,42 @@ def not_blank(question):
 
 # Loop to get ticket details
 
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+
+    # tell users how many seats are left
+    if count < 4:
+        print("You have {} seats "
+              "left".format(MAX_TICKETS - count ))
+
+    # warns users that only one seat is left
+    else:
+        print("*** There is one seat left!! ***")
+
+    # Get details...
+
     # Get name (Can't be blank)
     name = not_blank("Name: ")
+    count += 1
+    print()
+
+if count == MAX_TICKETS:
+    print("You have sold all the avaliable tickets! ")
+else:
+    print(" You have {} tickets.    \n"
+          "There are {} places still avaliable"
+          .format(count, MAX_TICKETS - count))
+
+    # Get name (Can't be blank)
+    name = not_blank("Name: ")
+
+# start of loop
+
+# initialise loop so that it runs at least once
+
 
     # Get age (Between 12 and 130)
 
